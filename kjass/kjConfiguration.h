aaee4cc1b2bfc8a2f7e66b12a48ef8b7edcf69c9
Волчачка Беловолк <54104335+volchachka@users.noreply.github.com>
2022-12-24 00:23:23 +0400

    update & fix KJ Config header ; )

--- kjass/kjConfiguration.h
+++ kjass/kjConfiguration.h
@@ -1,22 +1,40 @@
 namespace KingJass {
 
-    namespace ParseTokenType {
-        static const int StringOpen     = 0x0;
-        static const int StringClose    = 0x2;
-        static const int ScopeOpen      = 0x4;
-        static const int ScopeClose     = 0x8;
-        static const int CommentSingle  = 0x10;
-        static const int CommentOpen    = 0x20;
-        static const int CommentClose   = 0x40;
+    /*
+            In KingJass a Directive
+            code: {
+                @DIRECTIVE_DEBUG 
+            }
+            Presence of this directive enables debug mode 
+            Directive enables the mode matching to it.
+    */
+    namespace Directive {
+        
+        static const char Debug         [] = "DIRECTIVE_DEBUG";
+        static const char AutoLogger    [] = "DIRECTIVE_AUTO_LOG";
+        static const char Exceptions    [] = "DIRECTIVE_EXCEPT";
+        static const char Optimization  [] = "DIRECTIVE_OPTMIMIZE";
+
+        enum Flags { DEBUG = 0, AUTOLOG = 2, EXCEPTIONS = 4, OPTIMIZATIONS = 8  };
+        
     }
 
-    namespace Config
-    {
-        static const int Succeful       = 0x0;
-        static const int ErrorLine      = 0x2;
-        static const int ErrorFile      = 0x4;
-        static const int AccessDenied   = 0x8;
+    enum ParseTokenType {
+        StringOpen     = 0x0,
+        StringClose    = 0x2,
+        ScopeOpen      = 0x4,
+        ScopeClose     = 0x8,
+        CommentSingle  = 0x10,
+        CommentOpen    = 0x20,
+        CommentClose   = 0x40
+    };
 
-    } 
+    enum Config {
+        Succeful       = 0x0,
+        ErrorLine      = 0x2,
+        ErrorFile      = 0x4,
+        AccessDenied   = 0x8
+
+    };
     
 }
